3eb902da11a93e01951904543ef8a5c59c8bd901
TheFox <git@fox21.at>
2012-02-04 13:05:30 +0100

    Code cleanup and comments.

--- polymorphic.pl
+++ polymorphic.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl -w
 # Created @ 21.01.2012 by TheFox@fox21.at
 # Polymorphic Perl Script
+# Version: 0.2.0
 
 use strict;
 
@@ -15,7 +16,7 @@ $| = 1;
 
 
 # Variable blacklist
-my @bl = (
+my @nameBlacklist = (
 	'chdir',
 	'chmod',
 	'chr',
@@ -68,156 +69,72 @@ sub main{
 sub permute{
 	print "permute $0\n";
 	
-	my $s = '';
+	my $source = '';
 	
 	#while(1){ print strRand(numberRand(1, 40))."\n"; }
 
 	### CUT ###
-	$s = '#!/usr/bin/perl -w
+	$source = '#!/usr/bin/perl -w
 		# created
 
-#my $n = 0;
-#for(my $i = 0; $i < $n; $i++){
-#	my $x = substr $i;
-#	print "n = $i $n";
-#	
-#}
 
-main();
 
-sub main
- {
-my $n = 0;
-my $contlen = length $cont;
-for($n = 0; $n < $contlen; $n++){
-	print "n = $i $n";
-}
+use strict;
+$| = 1;
 
-my %h = ();
-for my $k (keys %h){ print "$k $h{$k}"; }
-}
-
-main();
+my $x = 0;
+print "\t$x\n";
 
 	';
 	### CUT ###
 	
 	
-	$s = fileRead($0);
+	
+	$source = fileRead($0);
+	#$source = fileRead($0.'.pl');
 	
 	
-	my $o = '';
+	my $out = '';
 	
 	
-	$s =~ s/\r//sg;
-	strTrim(\$s);
+	
+	strTrim(\$source);
 	
 	
-	my $rc = 0;
+	# Read row by row.
+	my $rowCounter = 0;
 	my $cutter = 0;
-	my @rows = split /\n/, $s;
+	my @rows = split /\n/, $source;
 	for my $row (@rows){
+		$rowCounter++;
 		
-		$rc++;
-		#$row =~ s/^([^\x23]*)$/$1/;
-		
-		
-		if($rc > 2){
+		if($rowCounter > 2){
 			$row =~ s/^[ \t]+//;
-			
-			
 			if($row =~ /\x23 CUT \x23/){
 				$cutter = !$cutter;
 			}
-			
-			
-			
 			if(!$cutter){
-				#print "r $rc '$row'\n";
-				
 				if($row ne '' && $row !~ /^\x23/){
 					
-					#map{ } @bl;
-					#my @tbl = grep{
-					#	$row !~ /^print / && $row !~ /^my /
-					#	&& $row =~ /\$/ && $row =~ /$_/
-					#} @bl;
-					#if(@tbl){
-					#	print STDERR "ERROR: Can't parse file in line $rc: '$row' (@tbl)\n";
-					#	exit(1);
-					#}
+					#if($row !~ / $/){ $row .= ' '; }
 					
-					
-					if($row !~ / $/){
-						$row .= ' ';
-					}
-					
-					$o .= $row;
+					$out .= $row;
 				}
 			}
 		}
-		
 	}
 	
 	#existrTrim();
 	#print "\n\n";
 	
 	
-	my $level = 0;
-	my $ol = length $o;
 	
-	
-	my $o2 = '';
-	
-	my $cmdid = 0;
-	my %cmds = ();
-	
-	
-	for(my $n = 0; $n < $ol; $n++){
-		
-		my $c = substr $o, $n, 1;
-		my $c4 = substr $o, $n, 4;
-		my $cn = ord($c);
-		
-		if($cn != 10 && $cn != 9){
-			
-			#print $c;
-			
-			
-			$o2 .= $c;
-			
-			
-			
-			
-			
-			if($c eq '{' || $c eq '('){
-				#printf "c '%4s' %3d ".("\t" x $level)." '$c' \n", $c4, $cn;
-				$level++;
-				
-			}
-			elsif($c eq '}' || $c eq ')'){
-				
-				$level--;
-				#printf "c '%4s' %3d ".("\t" x $level)." '$c' \n", $c4, $cn;
-				
-			}
-			else{
-				#printf "c '%4s' %3d ".("\t" x $level)." '$c' \n", $c4, $cn;
-			}
-			
-			
-			
-		}
-		
-	}
-	
-	
-	my $o3 = $o2;
-	#$o3 = cmdsexec($o2);
+	my $out3 = $out;
+	#$out3 = cmdsexec($out2);
 	
 	# Collect subs.
 	my %subs = ();
-	while($o3 =~ /sub ([a-z0-9_]+)/ig){
+	while($out3 =~ /sub ([a-z0-9_]+)/ig){
 		my $subname = $1;
 		strTrim(\$subname);
 		
@@ -225,24 +142,30 @@ main();
 		if(!defined $subs{$subname}){
 			#print "sub '$subname'\n";
 			
-			my $newname = 'sub_'.numberRand(100, 999).'_'.$subname.'_'.numberRand(100, 999);
+			my $newname = '';
+			#$newname = 'sub_'.numberRand(100, 999).'_'.$subname.'_'.numberRand(100, 999);
+			$newname = strRand(numberRand(2, 128));
 			$subs{$subname} = {
 				'name' => $subname,
 				'newname' => $newname,
 			};
 			
-			#$o3 =~ s/sub $subname/sub $newname/sg;
+			#$out3 =~ s/sub $subname/sub $newname/sg;
 		}
 	}
 	
 	# Collect variables.
 	my %vars = ();
-	while($o3 =~ /([\$\%\x40])([a-z0-9_]+)/ig){
+	while($out3 =~ /([\$\%\x40])([a-z0-9_]+)/ig){
 		
 		my $vartype = $1;
 		my $varname = $2;
 		
-		if( ($vartype eq '$' && $varname =~ /[ab0-9]/) || ($varname eq '_') ){
+		#print "var coll: '$vartype' '$varname' \n";
+		
+		#if( ($vartype eq '$' && $varname =~ /^[ab0-9]$/) || $varname eq '_' || $varname eq '|' ){
+		if( length $varname <= 2){
+			print "skip var: '$vartype' '$varname' \n";
 			next;
 		}
 		
@@ -250,148 +173,173 @@ main();
 						
 			my $newname = '';
 			#$newname = 'var_'.numberRand(100, 999).'_'.$varname.'_'.numberRand(100, 999);
-			$newname = 'var_'.numberRand(1, 9).'_'.$varname;
+			#$newname = 'var_'.numberRand(100, 999).'_'.$varname;
+			$newname = strRand(numberRand(2, 128));
 			$vars{$varname} = {
 				'name' => $varname,
 				'newname' => $newname,
 				'type' => $vartype,
 			};
+			
+			#print "\t\t\t\t\tnew: $newname\n";
 		}
+		#else{ print "\told: $vars{$varname}{'newname'}\n"; }
+		#print "\n";
+		
+		#sleep 1;
 	}
+	print "\n";
+	
+	# TODO: Mix subs
 	
 	# Substitute subs.
 	my @subskeys = keys %subs;
-	print "subs ".@subskeys."\n";
+	print "\nsubs ".@subskeys."\n";
 	for my $subname (reverse sort{
 		length($a) <=> length($b)
 	} @subskeys){
 		my $newname = $subs{$subname}{'newname'};
 		
 		
-		$o3 =~ s/sub $subname/sub $newname/g;
+		$out3 =~ s/sub $subname/sub $newname/g;
 		print "sub1 'sub $subname' 'sub $newname'\n";
 		
-		$o3 =~ s/$subname\(/$newname\(/g;
+		$out3 =~ s/$subname\(/$newname\(/g;
 		#print "sub1 '$subname(' '$newname('\n\n";
 		
 	}
 	
 	# Substitute variables.
 	my @varskeys = keys %vars;
-	print "vars ".@varskeys."\n";
+	print "\nvars ".@varskeys."\n";
 	for my $varname (reverse sort{
 		length($a) <=> length($b)
 	} @varskeys){
 		my $newname = $vars{$varname}{'newname'};
 		my $vartype = $vars{$varname}{'type'};
 		
-		if($vartype eq '%'){
-			$o3 =~ s/\$$varname\{/\$$newname\{/sg;
-			#print "var1 $vartype '\$$varname\{' '\$$newname\{'\n";
-		}
-		$o3 =~ s/\Q$vartype\E$varname/$vartype$newname/sg;
-		print "var1 $vartype '$vartype$varname' '$vartype$newname'\n";
+		#if($vartype eq '%'){
+		#	$out3 =~ s/\$$varname\{/\$$newname\{/sg;
+		#	print "var $vartype '\$$varname\{' '\$$newname\{'\n";
+		#}
+		#$out3 =~ s/\Q$vartype\E$varname/$vartype$newname/sg;
 		
-		#print "$o3\n\n"; sleep 1;
+		$out3 =~ s/([\$\%\x40])$varname/$1$newname/sg;
+		
+		print "var $vartype '$vartype$varname' '$vartype$newname'\n";
+		
+		#print "$out3\n\n"; sleep 1;
 	}
-	#replacePvarsAndLvars(\$o3, \%vars);
+	#replacePvarsAndLvars(\$out3, \%vars);
+	
+	
+	numberRand(1, 9999) >= 5000 ? $out3 =~ s/\\x0a/\\n/sg : $out3 =~ s/\\n/\\x0a/sg;
+	numberRand(1, 9999) >= 5000 ? $out3 =~ s/\\x09/\\t/sg : $out3 =~ s/\\t/\\x09/sg;
+	
 		
-	#$o3 =~ s/;/;\n/sg;
-	#$o3 =~ s/\}/\}\n/sg;$o3 =~ s/\{/\{\n/sg;
-	#$o3 =~ s/(sub [0-9a-z]+)/\n\n\n$1/sig;
+	#$out3 =~ s/;/;\n/sg;$out3 =~ s/\}/\}\n/sg;$out3 =~ s/\{/\{\n/sg;
+	#$out3 =~ s/(sub [0-9a-z]+)/\n\n\n$1/sig;
 	
-	#$s =~ s/\r//sg;$s =~ s/\n//sg;
+	#$source =~ s/\r//sg;$s =~ s/\n//sg;
 	
-	#print "\n\nout\n$o3\n";
+	#print "\n\nout\n$out3\n";
 	
-	my $f = $0.'.pl';
-	fileWrite($f, "\x23!/usr/bin/perl -w\n\x23 Created by TheFox\x40fox21.at\n\x23 ".time()."\n\n".$o3);
-	chmod 0755, $f;
+	my $outPath = $0.'.pl';
+	fileWrite($outPath, "\x23!/usr/bin/perl -w\n\x23 Created by TheFox\x40fox21.at\n\x23 ".time()."\n\n".$out3);
+	chmod 0755, $outPath;
 	
-	$f;
+	$outPath;
 }
 
 # Read a file
 sub fileRead{
-	my($f) = @_;
-	my $s = '';
+	my($path) = @_;
+	my $content = '';
 	
-	open F, '<', $f;
-	$s = join '', <F>;
+	open F, '<', $path;
+	$content = join '', <F>;
 	close F;
 	
-	$s;
+	$content;
 }
 
 # Write a file
 sub fileWrite{
-	my($f, $c) = @_;
-	open F, '>', $f;
-	print F $c;
+	my($path, $content) = @_;
+	open F, '>', $path;
+	print F $content;
 	close F;
 }
 
 # Random number: min, max
 sub numberRand{
-	my($mi, $ma) = @_;
-	int($mi + int rand($ma - $mi + 1));
+	my($min, $max) = @_;
+	int($min + int rand($max - $min + 1));
 }
 
 # Random string
 sub strRand{
-	my($len) = @_;
-	my $rv = '';
+	my($len, $level) = @_;
+	if(!defined $level){
+		$level = 0;
+	}
+	if($level >= 98){
+		return 'TheFoxStrikesBack';
+	}
+	
+	my $retVal = '';
 	
 	my $nlen = $len;
 	$nlen--;
-	$rv .= chr((numberRand(1, 1000) <= 800 ? 97 : 65) + int rand 26);
 	
-	for(my $n = 0; $n < $nlen; $n++){
-		$rv .= numberRand(1, 1000) <= 900 ? chr((numberRand(1, 1000) <= 800 ? 97 : 65) + int rand 26) : numberRand(1, 9);
+	# Sub names can't have a number at the first char.
+	$retVal .= chr((numberRand(1, 1000) <= 800 ? 97 : 65) + int rand 26);
+	
+	for(my $counter = 0; $counter < $nlen; $counter++){
+		$retVal .= numberRand(1, 1000) <= 900 ? chr((numberRand(1, 1000) <= 800 ? 97 : 65) + int rand 26) : numberRand(1, 9);
 	}
 	
 	
-	if(grep{ $rv =~ /$_/i } @bl){
-		$rv = strRand($len);
+	if(grep{ $retVal =~ /$_/i } @nameBlacklist){
+		$retVal = strRand($len, $level + 1);
 	}
 	
-	$rv;
+	$retVal;
 }
 
 # Trim String
 sub strTrim{
-	my($r) = @_;
+	my($strRef) = @_;
 	
-	$$r =~ s/^ +//s;
-	$$r =~ s/ +$//s;
+	$$strRef =~ s/^ +//s;
+	$$strRef =~ s/ +$//s;
 	
-	$$r =~ s/^\n+//s;
-	$$r =~ s/\n+$//s;
+	$$strRef =~ s/^\n+//s;
+	$$strRef =~ s/\n+$//s;
+	
+	$$strRef =~ s/\r//sg;
+	
 }
 
 # Merge two hashes to one
 sub hashMerge{
-	my($hr1, $hr2) = @_;
-	my %rv222 = ();
+	my($hashref1, $hashref2) = @_;
+	my %retVal = ();
 	
-	for my $hr ($hr1, $hr2){
-		if(defined $hr){
-			my %h = %{$hr};
-			for my $k (keys %h){
-				$rv222{$k} = $h{$k};
+	for my $hashref ($hashref1, $hashref2){
+		if(defined $hashref){
+			my %hash = %{$hashref};
+			for my $key (keys %hash){
+				$retVal{$key} = $hash{$key};
 			}
 		}
 	}
 	
-	%rv222;
+	%retVal;
 }
 
 
-
 main();
-
-
-
 
 
 # EOF
